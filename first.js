bcd9fad525c490fb6d2d0e9d172c9eba73a0a2d3
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2023-10-11 04:33:06 +0000

    new

--- first.js
+++ first.js
@@ -1,1 +1,19 @@
-console.log("ok");
+//first node js file i am start from hear
+
+
+let na="ashutosh"
+var email="ashu@gmail.com" 
+//Never use var we always use let for good prectice
+
+
+const mobileNo=926798282
+
+// without giving any data type variable is also initilise
+
+add="vijayNagar" 
+add1=2
+console.log(typeof (add))
+console.log(typeof(add1));  
+
+//console.table(na, email, mobileNo, add);
+console.log(email);
